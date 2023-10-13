8eca2fcf1317f4be807829f42f79655c47ff5d2b
Skyler Hochmuth <skyler.hochmuth@live.com>
2023-10-13 16:37:44 -0600

    Large Update to WebPage
    Setup the header, imported header icon, redid layout of the web page, created add book button, successfully hooked up JS to html to display books, setup book card layout, setup scroll grid display of books, created dynamic sidebar that pops out. will integrate add book form into sidebar next

--- app.js
+++ app.js
@@ -14,25 +14,50 @@ function Book(title, author, pageNumber, publishDate, readToggle) {
     // }
 }
 
-myLibrary.push(new Book('The Hobbit', 'JR Tolkien', '304', 'September 21, 1937', false))
+myLibrary.push(new Book('The Hobbit', 'JR Tolkien', 304, 'September 21, 1937', false))
+myLibrary.push(new Book('To Kill a Mockingbird', 'Harper Lee', 336, 'July 11, 1960', true ))
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+myLibrary.push(new Book())
+
+
+
 
 function addBookToLibrary(event) {
     myLibary.push(new Book(event.title));
     const string = JSON.stringify(myLibrary, null, 4);
     document.querySelector(".books").innerHTML = string;
-    event.preventDefault
+    event.preventDefault();
 }
+
+// waits for "Add Book" click to open up sidebar form
+window.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("button-71").addEventListener("click", () => {
+		const sidebarEl = document.getElementsByClassName("sidebar")[0];
+		sidebarEl.classList.toggle("sidebar--isHidden");
+	});
+});
 
 
 // document.getElementByName('Submit').addEventListener('click', addBookToLibrary(event))
-document.getElementsByClassName('books').innerHTML = myLibrary.map(book => 
-    <div>
-        <p>Title: ${book.title}</p>
-        <p>Author: ${book.author}</p>
-    </div>
-).join('');
-
-
-console.log(myLibrary.map(book => {
-    return book['title'];
-}))
+document.getElementById("books").innerHTML = myLibrary.map((book) => 
+    `<div class="book-card">
+        <div class="card-top">
+            <h3>${book.title}</h3>
+            <p class="card-pages">Pages: ${book.pageNumber}</p>
+            <p>by ${book.author}</p>
+        </div>
+        <hr></hr>
+        <div class="card-bottom">
+            <p>Published: ${book.publishDate}</p>
+            <p>${book.readToggle}</p>
+        </div>
+    </div>`
+).join('')
+    
