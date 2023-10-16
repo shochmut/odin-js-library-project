765c3b34acd03d02d4ad27681d26e2ac63058562
Skyler Hochmuth <skyler.hochmuth@live.com>
2023-10-15 23:18:57 -0600

    Added Form Submission Functionality
    still working on form submission linking data to card displays. Successfully displays data for new card submission now. Just need to link data up correctly next.

--- app.js
+++ app.js
@@ -16,24 +16,27 @@ function Book(title, author, pageNumber, publishDate, readToggle) {
 
 myLibrary.push(new Book('The Hobbit', 'JR Tolkien', 304, 'September 21, 1937', false))
 myLibrary.push(new Book('To Kill a Mockingbird', 'Harper Lee', 336, 'July 11, 1960', true ))
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
-myLibrary.push(new Book())
 
 
 
 
-function addBookToLibrary(event) {
-    myLibary.push(new Book(event.title));
-    const string = JSON.stringify(myLibrary, null, 4);
-    document.querySelector(".books").innerHTML = string;
-    event.preventDefault();
+
+function addBookToLibrary(title, author, pages, publicationDate, read) {
+    myLibrary.push(new Book(title, author, pages, publicationDate, read));
+    document.getElementById("books").innerHTML = myLibrary.map((book) => 
+    `<div class="book-card">
+        <div class="card-top">
+            <h3>${book.title}</h3>
+            <p class="card-pages">Pages: ${book.pageNumber}</p>
+            <p>by ${book.author}</p>
+        </div>
+        <hr></hr>
+        <div class="card-bottom">
+            <p>Published: ${book.publishDate}</p>
+            <p>${book.readToggle}</p>
+        </div>
+    </div>`
+).join('');
 }
 
 // waits for "Add Book" click to open up sidebar form
@@ -60,4 +63,24 @@ document.getElementById("books").innerHTML = myLibrary.map((book) =>
         </div>
     </div>`
 ).join('')
-    
+
+
+
+// event listener to handle add book submission
+document.getElementById('book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let title = document.getElementsByName('title');
+    let author = document.getElementsByName('author');
+    let pages = document.getElementsByName('pages');
+    let publicationDate = document.getElementsByName('publication-date');
+    let inputRead = document.getElementsByName('input-read');
+
+    addBookToLibrary(title, author, pages, publicationDate, inputRead)
+    console.log(title)
+});
+
+// document.getElementById("submit").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     addBookToLibrary(e);
+// })
+
