f76f96dd7b6fe7393e8a00d4ef73eea9739a0ac8
Skyler Hochmuth <skyler.hochmuth@live.com>
2023-10-16 15:07:56 -0600

    Added Remove Book and Toggle Read Status Buttons
    Added two new buttons. One that removed books from both the library array and the DOM when pressed. And another button that toggles the read status of the book in both the library array and in the DOM. Minor functionality of removing books needs troubleshooting to remap html button attribute for "data" to the correct index after removal of indices.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ myLibrary.push(new Book('To Kill a Mockingbird', 'Harper Lee', 336, 'July 11, 19
 
 function addBookToLibrary(title, author, pages, publicationDate, read) {
     myLibrary.push(new Book(title, author, pages, publicationDate, read));
-    document.getElementById("books").innerHTML = myLibrary.map((book) => 
+    document.getElementById("books").innerHTML = myLibrary.map((book, bookNumber) => 
     `<div class="book-card">
         <div class="card-top">
             <h3>${book.title}</h3>
@@ -33,11 +33,31 @@ function addBookToLibrary(title, author, pages, publicationDate, read) {
         <hr></hr>
         <div class="card-bottom">
             <p>Published: ${book.publishDate}</p>
-            <p>${book.readToggle}</p>
+            <div class="read-toggle-row">
+                <p>${book.readToggle}</p>
+            </div>
+            <button data="${bookNumber}" class='remove-book' name="${book.title}" id='remove-book' type="button">Remove Book</button>
+            <button data="${bookNumber}" class='toggle-read' id="toggle-read" type="button" value='toggleRead'>Toggle Read Status</button>
         </div>
     </div>`
 ).join('');
 }
+
+// event listener function to handle toggling read status button press and remove book button press
+document.getElementById('books').addEventListener('click', (e) => {
+    if (e.target && e.target.matches('button.toggle-read')) {
+        let index = e.target.attributes.data.value;
+        myLibrary[index].readToggle = !myLibrary[index].readToggle;
+        e.target.previousElementSibling.previousElementSibling.firstElementChild.innerHTML = myLibrary[index].readToggle;
+    }
+    if (e.target && e.target.matches('button.remove-book')) {
+        let index = e.target.attributes.data.value;
+        myLibrary.splice(index, 1);
+        e.target.parentElement.parentElement.remove();
+        console.log(myLibrary)
+    }
+})
+
 
 // waits for "Add Book" click to open up sidebar form
 window.addEventListener("DOMContentLoaded", () => {
@@ -48,8 +68,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// document.getElementByName('Submit').addEventListener('click', addBookToLibrary(event))
-document.getElementById("books").innerHTML = myLibrary.map((book) => 
+document.getElementById("books").innerHTML = myLibrary.map((book, bookNumber) => 
     `<div class="book-card">
         <div class="card-top">
             <h3>${book.title}</h3>
@@ -59,24 +78,28 @@ document.getElementById("books").innerHTML = myLibrary.map((book) =>
         <hr></hr>
         <div class="card-bottom">
             <p>Published: ${book.publishDate}</p>
-            <p>${book.readToggle}</p>
+            <div class="read-toggle-row">
+                <p>${book.readToggle}</p>
+            </div>
+            <button data="${bookNumber}" class='remove-book' name="${book.title}" id='remove-book' type="button">Remove Book</button>
+            <button data="${bookNumber}" class='toggle-read' id="toggle-read" type="button" value='toggleRead'>Toggle Read Status</button>
         </div>
     </div>`
-).join('')
+).join('');
 
 
 
 // event listener to handle add book submission
 document.getElementById('book-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    let title = document.getElementsByName('title');
-    let author = document.getElementsByName('author');
-    let pages = document.getElementsByName('pages');
-    let publicationDate = document.getElementsByName('publication-date');
-    let inputRead = document.getElementsByName('input-read');
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let publicationDate = document.getElementById('publication-date').value;
+    let inputRead = document.getElementById('input-read').value;
 
     addBookToLibrary(title, author, pages, publicationDate, inputRead)
-    console.log(title)
+    console.log(author)
 });
 
 // document.getElementById("submit").addEventListener("submit", (e) => {
